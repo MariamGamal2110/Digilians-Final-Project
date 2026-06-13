d7c308c87530b29c421192f8fae41c1a445fa9af
Mariam Gamal <msmg18122022@gmail.com>
2026-06-13 13:06:03 +0300

    add dot env backend variable3

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const apiBaseUrl = env.VITE_API_BASE_URL || 'http://localhost:5000/api'
-
+  const apiBaseUrl = env.VITE_API_BASE_URL 
   return {
     plugins: [react()],
     define: {
